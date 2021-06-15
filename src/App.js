73eada4a50aaf497ac1778af3dff6fd0df61984e
andrea ilieschi <andrea.ilieschi@gmail.com>
2021-06-15 20:53:36 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
+import initialContacts from "./contacts.json";
 import './App.css';
 
-function App() {
+const TableHeader = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+  )
 }
+
+const TableBody = (props) => {
+  const rows = props.contacts.map((row) => {
+    return (
+      <tr key={row.id}>
+        <td><img className="vip-pic" src={row.pictureUrl} alt="contact" /></td>
+        <td>{row.name}</td>
+        <td>{row.popularity.toFixed(2)}</td>
+      </tr>
+    )
+  })
+
+  return <tbody>{rows}</tbody>
+}
+
+
+
+
+
+class Table extends Component { // mejor contructor
+  
+  state = {
+    contacts: initialContacts.slice(0, 5),
+  };
+
+  handleRandom = () => {
+    const randomContact = initialContacts[Math.floor(Math.random() * initialContacts.length)];
+    const newState = [...this.state.contacts]
+
+    newState.push(randomContact)
+
+   this.setState({
+      contacts: newState
+    });
+  }
+
+  handleSortName = () => {
+    const copyOfContacts = [...this.state.contacts];
+
+    copyOfContacts.sort((a, b) => {
+      if (a.name > b.name) {
+        return 1;
+      } else if (a.name < b.name) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+
+    this.setState({
+      contacts: copyOfContacts
+    });
+  }
+
+    handleSortPop = () => {
+    const copyOfContacts = [...this.state.contacts];
+
+    copyOfContacts.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+
+    this.setState({
+      contacts: copyOfContacts,
+    });
+  };
+  
+  render() {
+    const {contacts} = this.state
+    return (
+      <>
+      <button onClick={this.handleRandom}>Add Random Contact</button>
+      <button onClick={this.handleSortName}>Sort by Name</button>
+      <button onClick={this.handleSortPop}>Sort by Popularity</button>
+      <table>
+        <TableHeader />
+        <TableBody contacts={contacts} />
+      </table>
+      </>
+    )
+  }
+}
+
+
+
+
+
+
+class App extends Component {
+  render() {
+    
+    return (
+      <div className="App">
+        <h1>IronContacts</h1>
+        <Table />
+      </div>
+    )
+  }
+}
+
+
+
+
 
 export default App;
